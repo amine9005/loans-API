3374fad295ad437cab770e76084c68763d56049f
amine9005 <amamselmitrader@gmail.com>
2023-09-06 21:42:23 +0100

    multer implementations

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -1,4 +1,32 @@
-import { createWriteStream, unlink } from "fs";
-import type { PathLike } from "fs";
-import type { Request } from "express";
-import { Multer } from "multer";
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    cb(null, Date.now() + ext);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  fileFilter: function (req, file, callback) {
+    if (
+      file.mimetype == "image/png" ||
+      file.mimetype == "image/jpg" ||
+      file.mimetype == "image/jpeg"
+    ) {
+      callback(null, true);
+    } else {
+      console.log("only jpg,png and jpeg files are supported");
+      callback(null, false);
+    }
+  },
+
+  limits: { fileSize: 1024 * 1024 * 2 },
+});
+
+export default upload;
--- src/routes/products.router.ts
+++ src/routes/products.router.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import controller from "../controllers/products.controller";
 import verified from "../middleware/verifyJWT";
-
+import upload from "../middleware/upload";
 const router = express.Router();
 
 router.get("/", verified, controller.getProducts);
-router.post("/add", verified, controller.addProduct);
+router.post(
+  "/add",
+  [verified, upload.single("thumbnail")],
+  controller.addProduct
+);
 router.get("/:id", verified, controller.getProductById);
 router.delete("/delete/:id", verified, controller.deleteProduct);
 router.put("/update/:id", verified, controller.updateProduct);
